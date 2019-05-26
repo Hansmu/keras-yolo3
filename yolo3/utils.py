982fe02b3cfe56aa32fa5fca2599c659115db2f5
hansmu <hans.miikael@gmail.com>
2019-05-26 13:07:50 +0300

    Remove debug

--- yolo3/utils.py
+++ yolo3/utils.py
@@ -35,10 +35,7 @@ def rand(a=0, b=1):
 
 def get_random_data(annotation_line, input_shape, random=True, max_boxes=20, jitter=.3, hue=.1, sat=1.5, val=1.5, proc_img=True):
     '''random preprocessing for real-time data augmentation'''
-    print('The annotation line: ' + annotation_line)
     line = annotation_line.replace('\n', '').split('$$')
-    print(*line)
-    print('Opening image: ' + line[0])
     image = Image.open(line[0])
     iw, ih = image.size
     h, w = input_shape
